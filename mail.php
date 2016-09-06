0d61047151b23a0dd1171e3b1a3ee7d4583faaa6
david liu <david liu>
2016-09-06 12:32:14 -0400

    Minor style changes

--- mail.php
+++ mail.php
@@ -18,7 +18,9 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    
+	header('Location: thank_you.html');
+	//echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
