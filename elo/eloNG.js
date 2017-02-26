340e93a3535db855b894f5ebecb71262ae6ae54f
David Liu <dvdgege5@hotmail.com>
2017-02-26 00:19:56 -0500

    Add parameters to formController function

--- elo/eloNG.js
+++ elo/eloNG.js
@@ -2,7 +2,7 @@
 ( function() {
 	var app = angular.module ( 'eloModule', [ ] );		// create module - eloModule
 	
-	app.controller ( 'formController', function(){
+	app.controller ( 'formController', function($scope, $http){
 		$http.get("fetchAllPublic.php")
    		.then(function (response) {$scope.persons = response.data.records;});
 	});
