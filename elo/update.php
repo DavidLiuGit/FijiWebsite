7a4b4edb9b79310af0abe9fa01f7268e5fbd34d4
Unknown <dvdgege5@hotmail.com>
2017-02-27 03:31:07 -0500

    Execute query in PHP; echo results

--- elo/update.php
+++ elo/update.php
@@ -12,53 +12,58 @@
 	@$winA = $request->winnerA;
 
 	echo "Data received: " . $idA1 . " " . $idB1 . ", winnerA: " .$winA;
-	
-	
+
+
 	// get current scores of players
 	$servername = "127.0.0.1";       	// localhost - server that runs this PHP file also has MySQL db
     $username = "tournamentAdmin";      // provide username
     $password = "prophet";             	// provide password
     $myDB = "tournamentDB";
-	
+
 	// create connection
     try {
-        $conn = new PDO ("mysql:host=$servername;dbname=myDB", $username, $password);
+        $conn = new PDO ("mysql:host=$servername;dbname=$myDB", $username, $password);
 
         // set PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        //echo " Connected successfully";
     }
     catch(PDOException $e){
-        echo "Connection failed: " . $e->getMessage();
+        echo " Connection failed: " . $e->getMessage();
     }
-	
-    $stmt = $conn->prepare("SELECT elo FROM tournament WHERE id=:$playerID");		// get ELO
-    $stmt->bindParam(':playerID', $id); 
-	
+
+    $stmt = $conn->prepare("SELECT elo FROM tournament WHERE id=:playerID");		// get ELO
+    $stmt->bindParam(':playerID', $idA1);
 	try{
 		$id = $idA1;						// player A1
+		$stmt->execute();
 		if ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-			$eloA1 = $row[0];
+			//echo " Query executed:". $row . " " . $row['elo'];
+			$eloA1 = $row['elo'];
 			$id = $idB1;					// player B1
 		}
-		
+
+		$stmt->execute();
 		if ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-			$eloB1 = $row[0];
+			$eloB1 = $row['elo'];
 			$id = $idA2;					// player A2
 		}
-		
+
+		$stmt->execute();
 		if ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-			$eloA2 = $row[0];
+			$eloA2 = $row['elo'];
 			$id = $idB2;					// player B2
 		}
-		
+
+		$stmt->execute();
 		if ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-			$eloB2 = $row[0];
+			$eloB2 = $row['elo'];
 		}
 	} catch ( PDOException $e ) {
 		echo "Score query unsuccesful!";
 	}
-	
+
 	echo "Scores fetched: " . $eloA1 . " " . $eloB1;
-	
+	$conn = null;
+
 ?>
