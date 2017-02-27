abf774cad729b72208a4fb98322dfdd743b4dd33
Unknown <dvdgege5@hotmail.com>
2017-02-27 04:16:58 -0500

    Update ELO formula; it now computes each player's new score

--- elo/update.php
+++ elo/update.php
@@ -11,7 +11,7 @@
 	@$idB2 = $request->selB2;
 	@$winA = $request->winnerA;
 
-	echo "Data received: " . $idA1 . " " . $idB1 . ", winnerA: " .$winA;
+	echo "Data received: " . $idA1 . "," . $idB1 . ", winnerA: " .$winA;
 
 	// initiaalize connection details
 	$servername = "127.0.0.1";       	// localhost - server that runs this PHP file also has MySQL db
@@ -32,7 +32,7 @@
     }
 
 	// get current ELO score of players in match
-    $stmt = $conn->prepare("SELECT elo FROM tournament WHERE id=:playerID");		
+    $stmt = $conn->prepare("SELECT elo FROM tournament WHERE id=:playerID");
     $stmt->bindParam(':playerID', $idA1);
 	try{
 		$id = $idA1;						// player A1
@@ -62,27 +62,34 @@
 	} catch ( PDOException $e ) {
 		echo "Score query unsuccesful!";
 	}
-	
-	echo ($idA1 && $idA2);
-	
-	
+
+
+
 	// compute average score for each team
 	$eloA = ($idA1 && $idA2) ? ($eloA1+$eloA2)/2 : $eloA1;
 	$eloB = ($idB1 && $idB2) ? ($eloB1+$eloB2)/2 : $eloB1;
-	
+
 	// compute expected: E = 1 / ( 1 + 10 ^ ( (R_opp - R_myTeam) / 400 ) )
-	
-	$E_a = 1 / ( 1 + pow ( 10, ($eloB - $eloA) / 400 );
-	$E_b = 1 / ( 1 + pow ( 10, ($eloA - $eloB) / 400 );
-	
+
+	$E_a = 1 / ( 1 + pow ( 10, ($eloB - $eloA) / 400 ) );
+	$E_b = 1 / ( 1 + pow ( 10, ($eloA - $eloB) / 400 ) );
+	//echo " Expected:" . $E_a . "," . $E_b;
+
+
 	// compute delta for each team: k ( S + E ),	where k is a constant defined below; S = { 1 for win, 0 for loss }
 	// WolframAlpha chart: plot 50(1- (1/(1+10^(x/400)))), 50(0- (1/(1+10^(x/400)))) for x = [-500,500] 				for k = 50
 	$k = 50;
-	$d_A = $k * ( ($winA)?1:0 - $E_a );
-	$d_B = $k * ( ($winA)?0:1 - $E_b );
-	
-	echo " Deltas:" . $d_a . " " . $d_b;
-	
+	$d_A = $k * ( (($winA) ? 1 : 0) - $E_a );
+	$d_B = $k * ( (($winA) ? 0 : 1) - $E_b );
+	echo " Deltas:" . $d_A . "," . $d_B;
+
+	// compute new scores for each individual player
+	$newA1 = $eloA1 + $d_A;
+	$newA2 = $eloA2 + $d_A;
+	$newB1 = $eloB1 + $d_B;
+	$newB2 = $eloB2 + $d_B;
+	echo " newScores:" . $newA1 .",". $newB1;
+
 	$conn = null;
 
 ?>
